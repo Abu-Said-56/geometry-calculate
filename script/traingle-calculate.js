cf66c7f1e650d7ea60e4108956d323ab0cadf6ba
ABU SAID <abusaid@ABUs-MacBook-Air.local>
2024-02-14 01:48:51 +0600

    third commit

---
+++ script/traingle-calculate.js
@@ -0,0 +1,12 @@
+// for traingle
+
+function traingleArea(){
+    // traingle base
+    const traingleBase = document.getElementById('traingle-base');
+    const traingleBaseText =traingleBase.value;
+    const base = parseFloat(traingleBaseText);
+    // traingle height
+    const traingleHeight = document.getElementById('traingle-height');
+    const traingleHeightText = traingleHeight.value;
+    const height = parseFloat(traingleHeightText);
+}
